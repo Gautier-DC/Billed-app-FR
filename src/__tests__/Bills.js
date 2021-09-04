93841c4d87c25a88014a3059b3ab08e650dba077
Gautier De Coninck <gautier.deconinck@gmail.com>
2021-09-05 00:03:00 +0200

    Added: test for bills, need to finish close the modal test

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -1,14 +1,32 @@
 import { screen } from "@testing-library/dom"
 import {setupLocaleStorage} from "../../setup-jest"
 import { bills } from "../fixtures/bills.js"
+import Bills from "../containers/Bills"
 import BillsUI from "../views/BillsUI.js"
 
+const onNavigate = (pathname) => {
+  document.body.innerHTML = ROUTES({ pathname })
+}
 setupLocaleStorage('Employee')
 
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
-    describe("and there are Bills", () => {
+    describe("And it is loading", () => {
+      test("It should display loading message", () => {
+        const html = BillsUI({ loading: true })
+        document.body.innerHTML = html
+        expect(screen.getAllByText('Loading...')).toBeTruthy()
+      })
+    })
+    describe("But an error occured", () => {
+      test("It should display error page", () => {
+        const html = BillsUI({ error: 'An error occured' })
+        document.body.innerHTML = html
+        expect(screen.getAllByText('Erreur')).toBeTruthy()
+      })
+    })
+    describe("And there are Bills", () => {
       test("Then bills should be ordered from earliest to latest", () => {
         const html = BillsUI({ data: bills })
         document.body.innerHTML = html
@@ -17,10 +35,41 @@ describe("Given I am connected as an employee", () => {
         expect(dates).toEqual(datesSorted)
       })
     })
-    test("Then bill icon in vertical layout should be highlighted", () => {
-      const html = BillsUI({ data: []})
-      document.body.innerHTML = html
-      //to-do write expect expression
+    describe("And there is a bill", () => {
+      describe("When I click on eye icon", () =>{
+        test("Then the new bill should open", ()=>{
+          const html = BillsUI({ data: bills })
+          document.body.innerHTML = html
+          const testBill = new Bills({document, onNavigate, firestore: null , localStorage: window.localStorage})
+          testBill.handleClickIconEye = jest.fn()
+          screen.getAllByTestId('icon-eye')[0].click()
+          expect(testBill.handleClickIconEye).toBeCalled()
+        })
+        test("Then the modal should display the attached file", ()=>{
+          const html = BillsUI({ data: bills })
+          document.body.innerHTML = html
+          const testBill = new Bills({document, onNavigate, firestore: null , localStorage: window.localStorage})
+          const iconEye = screen.getAllByTestId('icon-eye')[0]
+          $.fn.modal = jest.fn()
+          testBill.handleClickIconEye(iconEye)
+          expect($.fn.modal).toBeCalled()
+          expect(document.querySelector('.modal')).toBeTruthy()
+        })
+      })
+      describe("When I am in the modal", ()=> {
+        describe("When I click on the close button", ()=> {
+          test("It should close the modal", ()=> {
+            const html = BillsUI({ data: bills })
+            document.body.innerHTML = html
+            /* const testBill = new Bills({document, onNavigate, firestore: null , localStorage: window.localStorage})
+            const iconEye = screen.getAllByTestId('icon-eye')[0]
+            $.fn.modal = jest.fn()
+            testBill.handleClickIconEye(iconEye)
+            expect($.fn.modal).toBeCalled() */
+            expect(document.querySelector('.modal')).not.toBeTruthy()
+          })
+        })
+      })
     })
   })
 })
--- src/__tests__/VerticalLayout.js
+++ src/__tests__/VerticalLayout.js
@@ -5,11 +5,17 @@ import {setupLocaleStorage} from "../../setup-jest"
 setupLocaleStorage('Employee')
 
 describe('Given I am connected as Employee', () => {
-  test("Then Icons should be rendered", () => {
-    const html = VerticalLayout(120)
-    document.body.innerHTML = html
-    expect(screen.getByTestId('icon-window')).toBeTruthy()
-    expect(screen.getByTestId('icon-mail')).toBeTruthy()
+  describe('When I am on Bills page', () => {
+    test("Then Icons should be rendered", () => {
+      const html = VerticalLayout(120)
+      document.body.innerHTML = html
+      expect(screen.getByTestId('icon-window')).toBeTruthy()
+      expect(screen.getByTestId('icon-mail')).toBeTruthy()
+    })
+    test("Then bill icon in vertical layout should be highlighted", () => {
+      const html = BillsUI({ data: []})
+      document.body.innerHTML = html
+      //to-do write expect expression
+    })
   })
-
 })
